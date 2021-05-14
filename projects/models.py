894019f507193728ae6c174a9e79ca30ccf46400
Yared <yareddeltoro@gmail.com>
2021-05-14 13:10:41 -0500

    1

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ class Proyecto(models.Model):
     tecnologias = models.CharField(max_length=255, null=False)
     ano = models.IntegerField(null=False)
     imagen = models.ImageField(default='null', upload_to='Proyectos')
-    urlimg = models.CharField(max_length=255, null=False)
+    urlimg = models.CharField(max_length=255, null=False, default="null")
 
     class Meta:
         verbose_name="Proyecto"
