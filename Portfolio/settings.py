320c3943acc0e8e97c2089ebc1a1b9779ff1bc1e
Yared <yareddeltoro@gmail.com>
2021-05-14 12:44:28 -0500

    1

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
